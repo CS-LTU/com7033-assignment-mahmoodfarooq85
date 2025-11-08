5fae0639f77c9e1a7ac80375f276c4d23fadadc5
Mahmood Farooq <moongoldpk@gmail.com>
2025-11-08 13:31:17 +0000

    Added simple comments for testing explanation

--- test_app.py
+++ test_app.py
@@ -1,26 +1,28 @@
 import unittest
 from app import app
 
+# This file is for testing my Flask web application pages, and to make sure that all my pages (Home, About, Register) are working fine.
+
 class BasicTests(unittest.TestCase):
 
-    def setUp(self):
-        # Create a test client before each test
-        self.app = app.test_client()
-
-    # Check if the home page loads correctly
+    # This test is to  check if the home page loads correctly
     def test_home_page(self):
-        response = self.app.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
+        tester = app.test_client(self) # creates a fake browser to test my site
+        response = tester.get('/', content_type='html/text') # visits home page
+        self.assertEqual(response.status_code, 200) # 200 means page loaded successfully
 
-    # Check if the about page loads correctly
+    # This test to check if the about page loads correctly
     def test_about_page(self):
-        response = self.app.get('/about', content_type='html/text')
+        tester = app.test_client(self)
+        response = tester.get('/about', content_type='html/text') # visits about page
         self.assertEqual(response.status_code, 200)
 
-    # Check if the register page loads correctly
+    # This test is to check if the register page loads correctly
     def test_register_page(self):
-        response = self.app.get('/register', content_type='html/text')
+        tester = app.test_client(self)
+        response = tester.get('/register', content_type='html/text') # visits register page
         self.assertEqual(response.status_code, 200)
 
+# This part runs the tests when I use the command in terminal
 if __name__ == "__main__":
     unittest.main()
