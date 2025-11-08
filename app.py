5d0c53790c5466b40601886cb386145956e328a8
Mahmood Farooq <moongoldpk@gmail.com>
2025-11-08 15:56:53 +0000

    Final version - all routes tested and working

--- app.py
+++ app.py
@@ -10,15 +10,56 @@ app = Flask(__name__)
 # Giving Database file name
 DB_NAME = "users.db"
 
-# Next step is function to create database and users table
+# Function to create database and users table
 def init_db():
-    # Creating the database and users table if they do not exist
     with sqlite3.connect(DB_NAME) as conn:
         cur = conn.cursor()
         cur.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT UNIQUE NOT NULL,
-            password_hash TEXT NOT NULL
-        )
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                password_hash TEXT NOT NULL
+            )
         """)
+        conn.commit()
+
+
+# Route for home page
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+# Route for about page
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+# Route for register page
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+@app.route("/show_data")
+def show_data():
+    # This route will show few rows from stroke_data.csv
+
+    # 1) Read the CSV file into a pandas 
+    df = pd.read_csv("stroke_data.csv")
+
+    # 2) only the first 20 rows 
+    small_df = df.head(20)
+
+    # 3) Turning the DataFrame into an HTML table
+    # classes="data-table" is just a CSS class name (for styling later if I want)
+    # index=False hides the left index column (0,1,2,3...)
+    table_html = small_df.to_html(classes="data-table", index=False)
+
+    # 4) Send the table HTML into my data.html template as "table"
+    return render_template("data.html", table=table_html)
+
+# Run the Flask app
+if __name__ == "__main__":
+    init_db() # create the database if not exists
+    app.run(debug=True)
+
