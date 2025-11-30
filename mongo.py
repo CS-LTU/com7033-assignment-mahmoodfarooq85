4deb872b1420e8ce890b33aaee349cd1451250c7
Mahmood Farooq <moongoldpk@gmail.com>
2025-11-30 20:56:08 +0000

    Security: Cleaned repo, credentials now hidden in .env

--- mongo.py
+++ mongo.py
@@ -1,15 +1,19 @@
-from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
+# Load .env variables
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("MONGO_DB_NAME", "hospital_db")
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+users_collection = db["users"]
 
 try:
-    client = MongoClient(MONGO_URI)
-    db = client["hospital_db"] # database name
-    users_collection = db["users"] # collection name
-    print("MongoDB connection successful!")
+    client.admin.command("ping")
+    print("MongoDB connection OK")
 except Exception as e:
     print("MongoDB connection failed:", e)
